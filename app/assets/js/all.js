6140f123d9e62526c86b21095239390631e2e096
jocelyn20065 <jenny22twin1998@gmail.com>
2021-08-15 23:33:05 +0800

    updated

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,14 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+ 
+//Initialize Swiper
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // }, -->下方圓形的換頁效果，設計稿沒有，不用加。
+  });
